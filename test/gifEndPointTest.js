08b26abadfa3a98bb8efc76335755b80738d5c40
emmanuel <kipronofb@gmail.com>
2019-11-05 22:23:41 +0300

    test modify gif comment endpoint

--- test/gifEndPointTest.js
+++ test/gifEndPointTest.js
@@ -7,6 +7,9 @@ import app from "../app";
 const { expect } = chai;
 chai.use(chaiHttp);
 
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIzNDQxZWdkMWdkQDRiM2EzMTUyLmNvbSIsImlhdCI6MTU3Mjk3MDkxNCwiZXhwIjoxNTczMDU3MzE0fQ.A9a6QLgy2q8pDlvwDtF8CSDv5T-yXSJgWcOeXNCAvZg";
+let gifId;
+
 describe("Gifs Endpoint", () => {
   it("should create a new gif image",
     async () => {
@@ -14,8 +17,8 @@ describe("Gifs Endpoint", () => {
         .post("/api/v1/gifs")
         .set("Content-Type", "application/x-www-form-urlencoded")
         .field("title", "title")
-        .field("gif_comment", "a very long comment")
-        .field("user_id", 1)
+        .field("comment", "a very long comment")
+        .field("userId", 1)
         .attach("file",
           fs.readFileSync("/home/emmanuel/Documents/class/devCAndela/coding/capstone_project/DevC_capstone_project/backend/images/funny.jpg1572970658275.undefined"),
           "preview.png");
@@ -26,37 +29,51 @@ describe("Gifs Endpoint", () => {
       expect(response.body.data).to.have.property("title");
       expect(response.body.data).to.have.property("url");
       expect(response.body.data).to.have.property("gif_comment");
+      gifId = response.body.data.id;
     });
 
-  it("returns all gifs", (done) => {
-    chai
-      .request(app)
-      .get("/api/v1/gifs")
-      .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIzNDQxZWdkMWdkQDRiM2EzMTUyLmNvbSIsImlhdCI6MTU3Mjk3MDkxNCwiZXhwIjoxNTczMDU3MzE0fQ.A9a6QLgy2q8pDlvwDtF8CSDv5T-yXSJgWcOeXNCAvZg")
-      .send()
-      .end((err, res) => {
-        if (err) {
-          done(err);
-        }
-        expect(res).to.have.status(200);
-        expect(res.body.status).to.equals("success");
-        expect(res.body.data).to.be.an("array");
-        done();
-      });
-  });
+  it("returns all gifs",
+    async () => {
+      const response = await chai.request(app)
+        .get("/api/v1/gifs")
+        .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIzNDQxZWdkMWdkQDRiM2EzMTUyLmNvbSIsImlhdCI6MTU3Mjk3MDkxNCwiZXhwIjoxNTczMDU3MzE0fQ.A9a6QLgy2q8pDlvwDtF8CSDv5T-yXSJgWcOeXNCAvZg")
+        .send();
+      expect(response).to.have.status(200);
+      expect(response.body.status).to.equals("success");
+      expect(response.body.data).to.be.an("array");
+    });
 
   it("returns a gif by ID",
 
     async () => {
-      const gifId = 1;
-      const res = await chai.request(app)
+      const response = await chai.request(app)
         .get(`/api/v1/gifs/ ${gifId}`)
-        .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIzNDQxZWdkMWdkQDRiM2EzMTUyLmNvbSIsImlhdCI6MTU3Mjk3MDkxNCwiZXhwIjoxNTczMDU3MzE0fQ.A9a6QLgy2q8pDlvwDtF8CSDv5T-yXSJgWcOeXNCAvZg")
+        .set("Authorization", `Bearer ${token} `)
         .send();
 
-      expect(res).to.have.status(200);
-      expect(res.body.status).to.equals("success");
-      expect(res.body.data).to.be.an("array");
-      expect(res.body.data).to.be.lengthOf(1);
+      expect(response).to.have.status(200);
+      expect(response.body.status).to.equals("success");
+      expect(response.body.data).to.be.an("array");
+      expect(response.body.data).to.be.lengthOf(1);
+      expect(response.body.data[0]).to.have.nested.property("id");
+      expect(response.body.data[0]).to.have.property("user_id");
+      expect(response.body.data[0]).to.have.property("title");
+      expect(response.body.data[0]).to.have.property("url");
+      expect(response.body.data[0]).to.have.property("gif_comment");
+    });
+
+  it("modifies gif user comment",
+    async () => {
+      const response = await chai.request(app)
+        .patch(`/api/v1/gifs/${gifId}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          comment: "another comment",
+        });
+
+      expect(response).to.have.status(200);
+      expect(response.body.status).to.equals("success");
+      expect(response.body.status).to.equals("success");
+      expect(response.body.status).to.equals("success");
     });
 });
