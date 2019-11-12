a88575cea9432101bcea6de55aef68f9dfea0d14
emmanuel <kipronofb@gmail.com>
2019-11-12 09:44:08 +0300

    created edit article endpoint

--- routes/articles.js
+++ routes/articles.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/", articleControllers.createArticle);
 router.get("/", articleControllers.getAllArticle);
 router.get("/:id", articleControllers.getArticleById);
+router.patch("/:id", articleControllers.editArticle);
 
 export default router;
