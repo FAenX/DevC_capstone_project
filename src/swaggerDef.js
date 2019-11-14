c20ea19a44138544c5ba37a031e5dfc995cb5a0d
emmanuel <kipronofb@gmail.com>
2019-11-14 03:37:14 +0300

    documentation

--- src/swaggerDef.js
+++ src/swaggerDef.js
@@ -1,22 +1,27 @@
 import swaggerJSDoc from "swagger-jsdoc";
 
-const host = `http://${process.env.IP}:${process.env.PORT}`;
+const host = "https://glacial-sea-11269.herokuapp.com";
 
 const swaggerDefinition = {
 
   info: {
-    title: "DevC capstone project",
+    title: "DevC capstone Project - Teamwork",
     version: "1.0.0",
-    description: "Teamwork",
+    description: "Teamwork is an internal social network for employees of an organization. The goal of this application is to facilitate more interaction between colleagues and promote team bonding.",
   },
   host,
-  basePath: "/",
+  basePath: "/api/vi/",
   swagger: "2.0",
   paths: { },
   definitions: { },
   responses: { },
   parameters: { },
-  securityDefinitions: { },
+  securityDefinitions: {
+    BasicAuth: {
+      type: "http",
+      scheme: "basic",
+    },
+  },
 };
 
 const options = {
