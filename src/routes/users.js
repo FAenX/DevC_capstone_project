cb629a8c012399b1b7280ae2865d54b654440f6c
emmanuel <kipronofb@gmail.com>
2019-11-15 08:13:42 +0300

    super user

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 
 // user routes
-router.post("/create-user", userControllers.createUser);
+router.post("/create-user", auth.isStaff, userControllers.createUser);
 router.post("/signin", userControllers.token);
 router.get("/", auth.isStaff, userControllers.viewAllUsers);
 router.get("/:id", auth.verifyToken, userControllers.getUserById);
