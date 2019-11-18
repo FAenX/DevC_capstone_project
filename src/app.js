347b3da33e36d4b7d0f9e4c3b5ea2ed8152c6e22
emmanuel <kipronofb@gmail.com>
2019-11-19 01:29:02 +0300

    Api

--- src/app.js
+++ src/app.js
@@ -42,8 +42,8 @@ app.use("*", cloudinaryConfig);
 app.use(json());
 
 
-app.use("/api/v1/auth/", userRoutes);
-app.use("/api/v1/gifs/", gifRoutes);
-app.use("/api/v1/articles/", articleRoutes);
+app.use("/Api/v1/auth/", userRoutes);
+app.use("/Api/v1/gifs/", gifRoutes);
+app.use("/Api/v1/articles/", articleRoutes);
 
 export default app;
